f34e2f32154cf5afe7a909dbb37af60fb6ccaf81
David Lora <despinal0425@gmail.com>
2022-07-20 18:27:26 -0400

    added files and directory for the movie api app

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+// eslint-disable-next-line no-undef
+const api = config.MY_KEY;
+
+console.log(api);
+// eslint-disable-next-line no-undef
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//   </div>`;
+
+// eslint-disable-next-line no-undef
+// setupCounter(document.querySelector('#counter'));
